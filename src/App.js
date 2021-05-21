abeb53b89c150a54a82e1f1f9a25c2522dcf5c52
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-21 18:55:44 +0200

    change backgroundColor

--- src/App.js
+++ src/App.js
@@ -6,16 +6,31 @@ function App() {
       <header className="App-header">
        <h3>Baby Names Data</h3> 
       </header>
-      <div style={{border: ".2rem solid green"}}>
-        {BabyNamesData.sort((name1,name2)=>name1.name.localeCompare(name2.name)).map((el) => {
-          return (
-            <div style={{ border: ".1rem solid blue" }} key={el.id}>{el.name}</div>
-          )
-        })
-          }
+      <div style={{ display: "flex", flexWrap: "wrap" , height: "fit-content" ,border: ".2rem solid green"}}>
+        {BabyNamesData
+          .sort((name1, name2) => name1.name.localeCompare(name2.name))
+          .map((sortedName) => {
+
+            let color = sortedName.sex === "f" ? "#ff000080" : "#00ff0080";
+            
+            return (
+            <p style={
+              {
+                padding: ".5rem",
+                margin: ".2rem",
+                width: "fit-content",
+                backgroundColor: color,
+                textAlign: "center"
+              }}
+              key={sortedName.id}>
+              {sortedName.name}
+            </p>
+          )}
+          )        
+        }
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
