f36786869c16694f003fd6fd2d252d5b659b7507
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-27 19:58:48 +0200

    Level 3 - start of favourites

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function BabyNames({babyList}) {
+function BabyNames({ babyList }) {
 
   const babyFilterList = babyList
 
   const sortedNames = babyFilterList
     .sort((name1, name2) => name1.name.localeCompare(name2.name));
   
+  const favouriteBaby = (name) => console.log(name.target);
+  
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", height: "fit-content", border: ".2rem solid green" }}>
-      {sortedNames
-        .map((babyName) => {
+    <div>
+      <h3>Favourites</h3>
+      <div style={{ display: "flex", flexWrap: "wrap", height: "fit-content", border: ".2rem solid green" }}>
+        {sortedNames
+          .map((babyName) => {
 
-          let color = babyName.sex === "f" ? "#ff000080" : "#00ff0080";
+            let color = babyName.sex === "f" ? "#ff000080" : "#00ff0080";
 
-          return (
-            <p style={
-              {
-                padding: ".5rem",
-                margin: ".2rem",
-                width: "fit-content",
-                backgroundColor: color,
-                textAlign: "center"
-              }}
-              key={babyName.id}>
-              {babyName.name}
-            </p>
+            return (
+              <p style={
+                {
+                  padding: ".5rem",
+                  margin: ".2rem",
+                  width: "fit-content",
+                  backgroundColor: color,
+                  textAlign: "center"
+                }}
+                key={babyName.id}
+                onClick={favouriteBaby}
+              >
+                {babyName.name}
+              </p>
+            )
+          }
           )
         }
-        )
-      }
-    </div>
-  )
+      </div>
+
+    </div>)
 }
 
 export default BabyNames
